7cdb4a6fc2f0fa4067e08c36bee86f6c1e347c22
Sergey Gornih <seregaland@yandex.ru>
2024-08-19 10:30:39 +0300

    fix

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -4,11 +4,6 @@
 const ApiError = require('../error/ApiError')
 const path = require('path')
 
-// const soundNarush2 = require('./../assets/api/narushitel_2_ULEY.mp3');
-//const soundNarush = require('./../assets/api/narushitel_ULEY.mp3');
-
-const sound = require("sound-play");
-
 
 class FileController {
 
@@ -44,25 +39,7 @@ class FileController {
         //     return res.status(500).json(error.message);
         // }
     }
-
-    //запуск звуков:
-    async sendNarush(req, res) {
-        try {
-            console.log("запуск звука Нарушитель")
-            //const __dirname = path.resolve();
-            //const filePath = path.join(__dirname, "../assets/api/narushitel_ULEY.mp3");
-
-            const filePath = `https://proj.uley.team/upload/2024-01-25T08:13:03.985Z.mp3`
-
-            await sound.play(filePath);
-            console.log("done");
-
-           return res.status(200).json({message: "Succes"});
-        } catch (error) {
-            console.log(error.message)
-            return res.status(501).json({message: "Error"});
-        }
-    }
+    
 }
 
 module.exports = new FileController()
