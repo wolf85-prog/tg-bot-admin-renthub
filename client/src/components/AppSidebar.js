cb459f3c34c93244989a215315b0938be97689ef
Sergey <seregaland@yandex.ru>
2025-01-16 09:51:57 +0300

    fix menu

--- client/src/components/AppSidebar.js
+++ client/src/components/AppSidebar.js
@@ -20,6 +20,8 @@ import { useUsersContext } from "./../chat-app-new/context/usersContext";
 import CompIcon from 'src/assets/images/dashboard3.png'
 import ProjIcon from 'src/assets/images/projects.png'
 import ChatIcon from 'src/assets/images/chat.png'
+import SupportIcon from 'src/assets/images/support_icon.png'
+
 import { newPretendent, getCountMessage } from 'src/http/adminAPI'
 
 // sidebar nav config
@@ -171,7 +173,7 @@ const AppSidebar = () => {
       component: CNavLink,
       name: 'Профиль',
       icon: <CIcon icon={cilPeople} customClassName="nav-icon" />,
-      onClick: ()=>handleLinkClick2('https://proj.uley.team:3000/specialist'),
+      onClick: ()=>handleLinkClick2(`${host}/specialist`),
       style: {cursor: 'pointer'},
     },
     {
@@ -192,10 +194,11 @@ const AppSidebar = () => {
     
     {
       component: CNavLink,
-      name: 'Telegram',
-      //href: 'https://t.me/ULEY_Assistant',
-      icon: <CIcon icon={cilSend} customClassName="nav-icon" />,
-      onClick: ()=>handleLinkClick('https://t.me/ULEY_Assistant'),
+      name: 'Тех. поддержка',
+      //to: '/support',
+      onClick: ()=>handleLinkClick2('https://proj.uley.team:3001/support'),
+      icon: <img src={SupportIcon} style={{width: '21px', marginRight: '20px', marginLeft: '6px'}} />,
+      // badge: countMessageWork !== '0' ? {color: 'info', text: countMessageWork,} : "",
       style: {backgroundColor: '#0078d421', cursor: 'pointer'},
     },
     {
