a717557b592edc433cefb5f82a263963240932bd
Sergey <seregaland@yandex.ru>
2025-07-08 10:42:04 +0300

    fix fetch

--- server/controllers/telegramController.js
+++ server/controllers/telegramController.js
@@ -3,7 +3,7 @@ const ApiError = require('../error/ApiError')
 //const { Op } = require('sequelize')
 
 //fetch api
-const fetch = require('node-fetch');
+//const fetch = require('node-fetch');
 const axios = require("axios");
 
 
@@ -14,8 +14,8 @@ const $host = axios.create({
 })
 
 //socket.io
-//const {io} = require("socket.io-client")
-//const socketUrl = process.env.SOCKET_APP_URL
+const {io} = require("socket.io-client")
+const socketUrl = process.env.SOCKET_APP_URL
 
 class TelegramController {
 
@@ -37,105 +37,105 @@ class TelegramController {
     }
 
     //delete message
-    // async delMessageToTelegram(req, res) {
-    //     const {user, messageId} = req.body 
+    async delMessageToTelegram(req, res) {
+        const {user, messageId} = req.body 
 
-    //     try {   
-    //         const url_send_msg = `https://api.telegram.org/bot${token}/deleteMessage?chat_id=${user}&message_id=${messageId}`
+        try {   
+            const url_send_msg = `https://api.telegram.org/bot${token}/deleteMessage?chat_id=${user}&message_id=${messageId}`
                             
-    //         console.log("Удаление сообщения в телеграм...")
+            console.log("Удаление сообщения в телеграм...")
                             
-    //         const ressend = await $host.get(url_send_msg)
+            const ressend = await $host.get(url_send_msg)
 
-    //         return res.status(200).json(ressend.data);
-    //     } catch (error) {
-    //         return res.status(500).json(error.message);
-    //     }
-    // }
+            return res.status(200).json(ressend.data);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
 
-    // //send photo
-    // async sendPhotoToTelegram(req, res) {
-    //     const {user, image, keyboard} = req.body 
+    //send photo
+    async sendPhotoToTelegram(req, res) {
+        const {user, image, keyboard} = req.body 
 
-    //     try {   
-    //         const url_send_msg = `https://api.telegram.org/bot${token}/sendPhoto?chat_id=${user}&photo=${image}&reply_markup=${keyboard}`
+        try {   
+            const url_send_msg = `https://api.telegram.org/bot${token}/sendPhoto?chat_id=${user}&photo=${image}&reply_markup=${keyboard}`
                             
-    //         console.log("Отправка фото в телеграм...")
+            console.log("Отправка фото в телеграм...")
                             
-    //         const ressend = await $host.get(url_send_msg)
+            const ressend = await $host.get(url_send_msg)
 
-    //         return res.status(200).json(ressend.data);
-    //     } catch (error) {
-    //         return res.status(500).json(error.message);
-    //     }
-    // }
+            return res.status(200).json(ressend.data);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
 
-    // //send document
-    // async sendDocumentToTelegram(req, res) {
-    //     const {user, document, keyboard} = req.body 
+    //send document
+    async sendDocumentToTelegram(req, res) {
+        const {user, document, keyboard} = req.body 
 
-    //     try {   
-    //         const url_send_msg = `https://api.telegram.org/bot${token}/sendDocument?chat_id=${user}&document=${document}&reply_markup=${keyboard}`
+        try {   
+            const url_send_msg = `https://api.telegram.org/bot${token}/sendDocument?chat_id=${user}&document=${document}&reply_markup=${keyboard}`
                             
-    //         console.log("Отправка документы в телеграм...")
+            console.log("Отправка документы в телеграм...")
                             
-    //         const ressend = await $host.get(url_send_msg)
+            const ressend = await $host.get(url_send_msg)
 
-    //         return res.status(200).json(ressend.data);
-    //     } catch (error) {
-    //         return res.status(500).json(error.message);
-    //     }
-    // }
+            return res.status(200).json(ressend.data);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
 
-    // //send document form
-    // async sendDocumentFormToTelegram(req, res) {
-    //     const {form, headers} = req.body 
+    //send document form
+    async sendDocumentFormToTelegram(req, res) {
+        const {form, headers} = req.body 
 
-    //     try {   
+        try {   
                             
-    //         console.log("Отправка документа через форму в телеграм...")
+            console.log("Отправка документа через форму в телеграм...")
                             
-    //         const ressend = await $host.get(`https://api.telegram.org/bot${token}/sendDocument`, form, {headers: headers,})
+            const ressend = await $host.get(`https://api.telegram.org/bot${token}/sendDocument`, form, {headers: headers,})
 
-    //         return res.status(200).json(ressend.data);
-    //     } catch (error) {
-    //         return res.status(500).json(error.message);
-    //     }
-    // }
+            return res.status(200).json(ressend.data);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
 
-    // //send video
-    // async sendVideoToTelegram(req, res) {
-    //     const {user, video, keyboard} = req.body 
+    //send video
+    async sendVideoToTelegram(req, res) {
+        const {user, video, keyboard} = req.body 
 
-    //     try {   
-    //         const url_send_msg = `https://api.telegram.org/bot${token}/sendVideo?chat_id=${user}&video=${video}&reply_markup=${keyboard}`
+        try {   
+            const url_send_msg = `https://api.telegram.org/bot${token}/sendVideo?chat_id=${user}&video=${video}&reply_markup=${keyboard}`
                             
-    //         console.log("Отправка видео в телеграм...")
+            console.log("Отправка видео в телеграм...")
                             
-    //         const ressend = await $host.get(url_send_msg)
+            const ressend = await $host.get(url_send_msg)
 
-    //         return res.status(200).json(ressend.data);
-    //     } catch (error) {
-    //         return res.status(500).json(error.message);
-    //     }
-    // }
+            return res.status(200).json(ressend.data);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
 
-    // //send audio
-    // async sendAudioToTelegram(req, res) {
-    //     const {user, audio, keyboard} = req.body 
+    //send audio
+    async sendAudioToTelegram(req, res) {
+        const {user, audio, keyboard} = req.body 
 
-    //     try {   
-    //         const url_send_msg = `https://api.telegram.org/bot${token}/sendAudio?chat_id=${user}&audio=${audio}&reply_markup=${keyboard}`
+        try {   
+            const url_send_msg = `https://api.telegram.org/bot${token}/sendAudio?chat_id=${user}&audio=${audio}&reply_markup=${keyboard}`
                             
-    //         console.log("Отправка аудио в телеграм...")
+            console.log("Отправка аудио в телеграм...")
                             
-    //         const ressend = await $host.get(url_send_msg)
+            const ressend = await $host.get(url_send_msg)
 
-    //         return res.status(200).json(ressend.data);
-    //     } catch (error) {
-    //         return res.status(500).json(error.message);
-    //     }
-    // }
+            return res.status(200).json(ressend.data);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
 
 }
 
